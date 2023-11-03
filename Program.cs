9c6349e7d0ad833d657e59da453a28d92ca961ac
Ongax <jhoseppulido1129@gmail.com>
2023-11-03 17:45:26 -0500

    feat: :rocket: Zipper

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO.Compression;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string EntryPath = "C:/Example/EntryPath";
+        string OutputPath = "C:/Example/OutputPath";
+        if (!Directory.Exists(OutputPath)){
+            Directory.CreateDirectory(OutputPath);
+        }
+        var directiories = Directory.EnumerateDirectories(EntryPath);
+        
+        Console.Clear();
+        Console.WriteLine("Zipping Initiation\n\n");
+        foreach (var directiory in directiories){
+            string zipName = $"{OutputPath}/{directiory.Remove(0, EntryPath.Length + 1)}.zip";
+            Console.WriteLine($"Zipping: {zipName}");
+            ZipFile.CreateFromDirectory(directiory,zipName);
+            Console.WriteLine("Zip done\n");
+        }
+        Console.Clear();
+        Console.WriteLine("Zipping done, thanks for using");
+        //Made by Ongax
+    }
+}
